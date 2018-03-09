711d6365aa60b0304c8befec96ddac5283519db5
sadiyaaysha <32979416+sadiyaaysha@users.noreply.github.com>
2018-03-09 23:33:11 +0530

    Create 5.java

---
+++ 5.java
@@ -0,0 +1,25 @@
+V/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes hereint n;
+     Scanner s=new Scanner(System.in);
+     int n;
+     n=s.nextInt();
+     int m;
+     m=s.nextInt();
+     int c=n*m;
+     if(c%2==0)
+     {
+     	System.out.print("even");
+     }
+     
+	}
+}
